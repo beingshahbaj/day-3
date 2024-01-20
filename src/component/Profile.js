c298322c678f6582d8ab8b08d4288d3ea60f3dde
beingshahbaj <130038256+beingshahbaj@users.noreply.github.com>
2024-01-20 16:56:42 +0530

    update

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 
 function Profile() {
   useEffect(() => {
-    // Fetch the user details from the API
+    
     fetch(`https://dummyjson.com/users/${localStorage.getItem("user").id}`)
       .then((res) => res.json())
       .then((data) => {
-        // Save the user object with all the details in local storage
+        
         localStorage.setItem("user", JSON.stringify(data));
       });
   }, []);
@@ -19,7 +19,7 @@ function Profile() {
         {localStorage.getItem("user").lastName}
       </p>
       <p>Email: {localStorage.getItem("user").email}</p>
-      {/* Display other user information */}
+    
     </div>
   );
 }
